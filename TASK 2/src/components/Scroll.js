99b07dc1ab8180c08c822220b6eb93e3c3bd821a
Bhagyashree13820 <104613784+Bhagyashree13820@users.noreply.github.com>
2022-04-30 23:24:36 +0530

    Create Scroll.js

---
+++ TASK 2/src/components/Scroll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return(
+        
+        <div style={{height: 'auto'}}>
+            {props.children}
+        </div>
+    )
+};
+
+export default Scroll;
